52ba375675a4a73f50ba40dd201a8cd87593eee4
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-02-24 21:44:20 +0500

    API  changes

--- src/login.tsx
+++ src/login.tsx
@@ -17,7 +17,7 @@ function Login() {
   setLoading(true);
 
   try {
-    const res = await fetch("https://shoppingstore-backend.vercel.app//api/auth/login", {
+    const res = await fetch("https://shoppingstore-backend.vercel.app/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ emailOrPhone, password }),

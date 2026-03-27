a6f2e300c3cb195ce3e8e74dda077e582bb61c14
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-03-28 01:13:14 +0500

    update the  cod

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ export default function App() {
         <Route path="/check_out" element={<Checkout />} />
         <Route path="/view_item" element={<Viewitem />} />
         <Route path="/success" element={<PaymentSuccess />} />
-        <Route path="/orderTracking/:id" element ={<OrderTracking/>}/>
+        <Route path="/orderTracking" element ={<OrderTracking/>}/>
 
       </Routes>
       <ScrollToTop />

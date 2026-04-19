7058e23644f857596c488432b988cf1614a19aea
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-04-19 22:05:02 +0500

    feaT: successfully  Products UPDATED successfully  implemented

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,13 +1,18 @@
 // src/api/axiosInstance.ts
 import axios from 'axios';
+const isDev = import.meta.env.DEV;
 
-const BASE = import.meta.env.VITE_API_BASE_URL;
+// Define your URLs clearly
+const LOCAL_API = "http://192.168.18.40:5173"; // Your local backend IP/Port
+const VITE_API_BASE_URL = "https://shoppingstore-backend.vercel.app"; // Your Vercel backend
+
+// Automatically choose the base
+const BASE = isDev ? LOCAL_API : VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
-  baseURL: `${BASE}/api`,   // → http://192.168.18.40:5000/api
+  baseURL: `${BASE}/api`, 
   timeout: 10000,
 });
-
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');

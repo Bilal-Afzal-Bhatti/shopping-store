e19cfc50ef44a18bc04d86d7ef7cc05a9b70fb66
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-04-19 11:17:06 +0500

    feaT: successfully  Products UPDATED successfully  implemented

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const BASE = 
-  import.meta.env.VITE_API_BASE_URL     ||
+  import.meta.env.VITE_API_BASE_URL||
   import.meta.env.VITE_API_FALLBACK_1   ||
   import.meta.env.VITE_API_FALLBACK_2   ;
 
--- src/login.tsx
+++ src/login.tsx
@@ -10,7 +10,7 @@ function Login() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_API_FALLBACK_1 || import.meta.env.VITE_API_FALLBACK_2; // Fallback to localhost if env var is missing
 
  
   // --- MANUAL LOGIN (For Email/Password users) ---
